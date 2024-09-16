a78129d1c4339d4ac02874844dc3598f9065adcc
anggarateo <114375533+anggarateo@users.noreply.github.com>
2024-09-16 16:02:19 +0700

    Update birthYear.ts: export default

--- utils/birthYear.ts
+++ utils/birthYear.ts
@@ -1,7 +1,9 @@
-export function birthYear (value?: string) {
+function birthYear (value?: string) {
   let result = value
   result = value.replace('BBY', 'Before the Battle of Yavin')
   result = value.replace('ABY', 'After the Battle of Yavin')
 
   return result
 }
+
+export default birthYear
